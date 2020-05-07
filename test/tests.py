ee1edbad193969138398f314d5ea1a4c5e141021
nathanbellalou <nathanb@campus.technion.ac.il>
2020-05-07 23:04:03 +0300

    test commit

--- test/tests.py
+++ test/tests.py
@@ -18,10 +18,8 @@ nasdaqEventStreamFrequencyTailored = file_input("test/EventFiles/NASDAQ_FREQUENC
 nasdaqEventStream_AAPL_AMZN_GOOG = file_input("test/EventFiles/NASDAQ_AAPL_AMZN_GOOG.txt", MetastockDataFormatter())
 nasdaqEventStream = file_input("test/EventFiles/NASDAQ_LONG.txt", MetastockDataFormatter())
 
-print("help")
+print("test nathan")
 
-
-#test nathan
 def closeFiles(file1, file2):
     file1.close()
     file2.close()
