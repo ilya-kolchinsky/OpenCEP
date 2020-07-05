d969537918fd342667750d26c2426643dd01ff5b
yuvadius <yuvalgliz@gmail.com>
2020-07-05 22:43:01 +0300

    Finished strict policy

--- base/Pattern.py
+++ base/Pattern.py
@@ -2,7 +2,7 @@ from base.Formula import Formula, EqFormula, IdentifierTerm, MinusTerm, AtomicTe
 from base.PatternStructure import PatternStructure
 from datetime import timedelta
 from misc.StatisticsTypes import StatisticsTypes
-from base.PatternStructure import SeqOperator
+from base.PatternStructure import SeqOperator, QItem
 from misc.ConsumptionPolicies import ConsumptionPolicies
 
 class Pattern:
@@ -16,25 +16,7 @@ class Pattern:
     tree construction mechanisms - this is hopefully a temporary hack.
     """
     def __init__(self, pattern_structure: PatternStructure, pattern_matching_condition: Formula = None,
-                 time_window: timedelta = timedelta.max, consumption_policies: ConsumptionPolicies = None):
-        #Check that skip policies are enforced only on SeqOperator
-        if consumption_policies and consumption_policies.skip:
-            if(pattern_structure.get_top_operator() != SeqOperator):
-                raise Exception("Skip policies only work with SeqOperator")
-        #Adjust formula if qitems are "strict"
-        if consumption_policies and consumption_policies.strict:
-            args = pattern_structure.args
-            for event_name in consumption_policies.strict:
-                for i in range(len(args) - 1):
-                    if(args[i + 1].name == event_name):
-                        pattern_matching_condition = AndFormula(
-                            pattern_matching_condition,
-                            EqFormula(
-                                IdentifierTerm(args[i].name, lambda x: x["Index"]), 
-                                MinusTerm(IdentifierTerm(args[i + 1].name, lambda x: x["Index"]), AtomicTerm(1))
-                            )
-                        )
-        
+                 time_window: timedelta = timedelta.max, consumption_policies: ConsumptionPolicies = None):        
         self.structure = pattern_structure
         self.condition = pattern_matching_condition
         self.window = time_window
@@ -42,6 +24,33 @@ class Pattern:
         self.statistics = None
         self.consumption_policies = consumption_policies
 
+        if consumption_policies:
+            #Check that skip policies are enforced only on SeqOperator
+            if consumption_policies.skip:
+                if(pattern_structure.get_top_operator() != SeqOperator):
+                    raise Exception("Skip policies only work with SeqOperator")
+            #Adjust formula if qitems are "strict"
+            if consumption_policies.strict:
+                self.set_strict_formulas(pattern_structure)
+
     def set_statistics(self, statistics_type: StatisticsTypes, statistics: object):
         self.statistics_type = statistics_type
         self.statistics = statistics
+
+    def set_strict_formulas(self, pattern_structure: PatternStructure):
+        #Adjust formula if qitems are "strict"
+        args = pattern_structure.args
+        for i in range(len(args)):
+            if(args[i].get_top_operator() == SeqOperator):
+                self.set_strict_formulas(args[i])
+        if(pattern_structure.get_top_operator() == SeqOperator):
+            for event_name in self.consumption_policies.strict:
+                for i in range(len(args) - 1):
+                    if(args[i + 1].name == event_name and args[i].get_top_operator() == QItem and args[i + 1].get_top_operator() == QItem):
+                        self.condition = AndFormula(
+                            self.condition,
+                            EqFormula(
+                                IdentifierTerm(args[i].name, lambda x: x["Index"]), 
+                                MinusTerm(IdentifierTerm(args[i + 1].name, lambda x: x["Index"]), AtomicTerm(1))
+                            )
+                        )
