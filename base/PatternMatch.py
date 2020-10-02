8bddfd4d4a809521f55455a49a5f9f60dc1043c5
Adir Gigi <adirgigi@campus.technion.ac.il>
2020-10-02 11:34:12 +0300

    added pattern id and changed the way a match is printed

--- base/PatternMatch.py
+++ base/PatternMatch.py
@@ -12,13 +12,26 @@ class PatternMatch:
         self.events = events
         self.last_timestamp = max(events, key=lambda x: x.timestamp).timestamp
         self.first_timestamp = min(events, key=lambda x: x.timestamp).timestamp
+        self.pattern_id = []
 
     def __eq__(self, other):
-        return isinstance(other, PatternMatch) and set(self.events) == set(other.events)
+        return isinstance(other, PatternMatch) and set(self.events) == set(other.events) and self.pattern_id == other.pattern_id
 
     def __str__(self):
         result = ""
+        match = ""
         for event in self.events:
-            result += "%s\n" % event
-        result += "\n"
+            match += "%s\n" % event
+        if len(self.pattern_id) == 0:
+            result += match
+            result += "\n"
+        else:
+            for idx in self.pattern_id:
+                result += "%s: " % idx
+                result += match
+                result += "\n"
         return result
+
+    def add_pattern_id(self, idx: int):
+        self.pattern_id.append(idx)
+
