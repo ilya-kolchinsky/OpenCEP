6a33f1c299c416a810c08cb717dc4fc520d86803
Adir Gigi <adirgigi@campus.technion.ac.il>
2020-10-08 22:10:46 +0300

    implemented sharing subtrees functions. passed a couple of tests :)

--- tree/MultiPatternTree.py
+++ tree/MultiPatternTree.py
@@ -7,6 +7,7 @@ from tree.Node import PrimitiveEventDefinition, Node
 from tree.UnaryNode import UnaryNode
 from tree.BinaryNode import BinaryNode
 from tree.NegationNode import NegationNode
+from tree.LeafNode import LeafNode
 from tree.PatternMatchStorage import TreeStorageParameters
 from plan.TreePlan import TreePlan
 
@@ -79,6 +80,7 @@ class MultiPatternTree:
                         pattern_id = list(leaf.get_pattern_id())[0]
                         self.__pattern_to_root_dict[pattern_id] = our_leaf
                         curr_root = []
+                        our_leaf.set_is_root(True)
 
                     leaves_to_counter_dict[dict_leaf] += 1
 
@@ -90,20 +92,73 @@ class MultiPatternTree:
     def __construct_subtrees_union_tree(self, tree_plans: List[TreePlan], patterns: List[Pattern],
                                         storage_params: TreeStorageParameters):
         trees = [Tree(tree_plans[i], patterns[i], storage_params, i+1) for i in range(len(patterns))]
-        roots = trees[0].get_roots()
+        self.__roots = []
         for i in range(len(patterns)):
             node = trees[i].get_root()
             index = i+1
-            for root in roots:
+            self.__roots.append(node)
+            self.__pattern_to_root_dict[index] = node
+            for root in self.__roots:
+                if root == node:
+                    break
+                if self.try_and_merge(root, node, index):
+                    break
+        return self.__roots
 
-               while not find_and_merge_node_into_subtree(root, node, index):
-                   if isinstance(node, BinaryNode):
-                        node = node.get_left_subtree()
-                        node = node.get_right_subtree()
-                   if isinstance(node, UnaryNode):
-                        node = node.get_child()
+    def try_and_merge(self, root, node, index):
+        if self.find_and_merge_node_into_subtree(root, node, index):
+            return True
+        if isinstance(node, BinaryNode):
+            left_merge = self.try_and_merge(root, node.get_left_subtree(), index)
+            right_merge = self.try_and_merge(root, node.get_right_subtree(), index)
+            return left_merge or right_merge
+        if isinstance(node, UnaryNode):
+            return self.try_and_merge(root, node.get_child(), index)
+        return False
 
-        return roots
+    def find_and_merge_node_into_subtree(self, root: Node, node: Node, index):
+        # in this check we make sure that we are not checking shared nodes more than once
+        # if len(root.get_pattern_id()) > 1 and min(root.get_pattern_id()) < index:
+        #    return False
+        if root.is_equal(node):
+            self.merge_nodes(root, node)
+            return True
+        elif isinstance(root, BinaryNode):
+            return self.find_and_merge_node_into_subtree(root.get_left_subtree(), node, index) or \
+                   self.find_and_merge_node_into_subtree(root.get_right_subtree(), node, index)
+        elif isinstance(root, UnaryNode):
+            return self.find_and_merge_node_into_subtree(root.get_child(), node, index)
+        return False
+
+    def merge_nodes(self, node: Node, other: Node):
+        #merges other into node
+        if node.get_sliding_window() < other.get_sliding_window():
+            node.update_sliding_window(other.get_sliding_window())
+        node.add_pattern_id(other.get_pattern_id())
+        other_parents = other.get_parents()
+        if other_parents is not None:
+            for parent in other_parents:
+                # assuming each node is either LeafNode or InternalNode and therefore has a get_event_definitions() method
+                if isinstance(other, LeafNode):
+                    event_defs = other.get_event_definitions()[0]
+                else:
+                    event_defs = other.get_event_definitions()
+
+                node.add_to_dict(parent,event_defs)
+                node.add_to_parent_to_unhandled_queue_dict(parent, Queue())
+                if isinstance(parent, UnaryNode):
+                    parent.replace_subtree(node)
+                elif isinstance(parent, BinaryNode):
+                    parent.replace_subtree(other, node)
+        else:
+            # other is a root in it's tree. the new root of the old_tree is node
+            if not node.is_root():
+                node.set_is_root(True)
+                self.__roots.append(node)
+                #other is already in self.__roots, therefore we need to remove it
+            self.__roots.remove(other)
+            other_id = list(other.get_pattern_id())[0]
+            self.__pattern_to_root_dict[other_id] = node
 
     def get_leaves(self):
         leaves = set()
@@ -144,19 +199,6 @@ class MultiPatternTree:
 This method gets _root and _node, and tries to find a node which is
 equivalent to _node in the subtree which _root is its root. If a node is found, it merges them.
 """
-def find_and_merge_node_into_subtree(root: Node, node: Node, index):
-    # in this check we make sure that we are not checking shared nodes more than once
-    if min(root.get_pattern_id()) < index:
-        return False
-    if root.is_equal(node):
-        root.unite(node)
-        return True
-    elif isinstance(root, BinaryNode):
-        return find_and_merge_node_into_subtree(root.get_left_subtree(), node, index) or \
-              find_and_merge_node_into_subtree(root.get_right_subtree(), node, index)
-    elif isinstance(root, UnaryNode):
-        return find_and_merge_node_into_subtree(root.get_child(), node, index)
-    return False
 
 
 
