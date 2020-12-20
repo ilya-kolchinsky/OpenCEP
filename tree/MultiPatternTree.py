82053327534f2803e6d203f5026a6d51278e4afe
twyair <yairt16@gmail.com>
2020-12-20 17:18:59 +0200

    add a confidence check for output nodes

--- tree/MultiPatternTree.py
+++ tree/MultiPatternTree.py
@@ -182,7 +182,10 @@ class MultiPatternTree:
                         continue
                     # check if timestamp is correct for this pattern id.
                     # the pattern indices start from 1.
-                    if match.last_timestamp - match.first_timestamp <= self.__id_to_pattern_map[pattern_id].window:
+                    pattern = self.__id_to_pattern_map[pattern_id]
+                    if match.last_timestamp - match.first_timestamp <= pattern.window and (
+                        pattern.confidence is None or match.probability >= pattern.confidence
+                    ):
                         match.add_pattern_id(pattern_id)
                 matches.append(match)
         return matches
