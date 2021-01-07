349b791a236245004801ef7c0ff35e18d0d959f9
ofriol <ofriol15@gmail.com>
2021-01-07 14:35:52 +0200

    Invariants - create 2 classes of invariants for both Zstream tree builder and greedy tree builder.

---
+++ plan/Invariants.py
@@ -0,0 +1,85 @@
+from abc import ABC
+
+from plan.InvariantBushyTreeBuilder import InvariantAwareZStreamTreeBuilder
+from base.Pattern import Pattern
+
+
+class Invariant:
+    """
+    This class represent single invariant.
+    invariant will be defined as an inequality of the following form:
+    f1 (stat1) < f2 (stat2)
+    Hence we save the index corresponding to stat1 in left side of the inequality
+    and index corresponding to stat2 in right side of the inequality
+    """
+
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+
+class Invariants(ABC):
+    """
+    This class represent a container for all invariants
+    """
+
+    def __init__(self):
+        self.invariants = []
+
+    def add(self, invariant):
+        self.invariants.append(invariant)
+
+
+class GreedyTreeInvariants(Invariants):
+    """
+    Check every invariant in invariants with next condition:
+    change_factor(invariant.left) < change_factor(invariant.right).
+    The verification proceeds in a bottom up order
+    """
+
+    def is_invariants_violated(self, pattern: Pattern):
+
+        arrival_rates = pattern.statistics.arrival_rates
+        selectivity_matrix = pattern.statistics.selectivity_matrix
+
+        first_index, second_index = self.invariants[0].left, self.invariants[0].right
+
+        first_change_factor = selectivity_matrix[first_index][first_index] * arrival_rates[first_index]
+        second_change_factor = selectivity_matrix[second_index][second_index] * arrival_rates[second_index]
+        if first_change_factor > second_change_factor:
+            return True
+
+        for i in range(1, len(self.invariants)):
+
+            first_change_factor = second_change_factor
+            first_change_factor *= selectivity_matrix[second_index][first_index]
+
+            first_index = second_index
+            second_index = self.invariants[i].right
+
+            # compute the second change factor
+            second_change_factor = selectivity_matrix[second_index][second_index] * arrival_rates[second_index]
+            for j in range(0, i):
+                second_change_factor *= selectivity_matrix[second_index][self.invariants[j].left]
+
+            if first_change_factor > second_change_factor:
+                return True
+
+        return False
+
+
+class ZStreamTreeInvariants(Invariants):
+    """
+    Check every invariant in invariants with next condition:
+    cost(invariant.left) < cost(invariant.right).
+    """
+
+    def is_invariants_violated(self, pattern: Pattern):
+
+        for (left, right) in self.invariants:
+            left_cost = InvariantAwareZStreamTreeBuilder._get_plan_cost(pattern, left)
+            right_cost = InvariantAwareZStreamTreeBuilder._get_plan_cost(pattern, right)
+            if left_cost >= right_cost:
+                return True
+
+        return False
