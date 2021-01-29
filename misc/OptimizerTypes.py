f766b0edad4ed8e35d8f6dffd8097eb5ca9da5ba
Robert Spektor <goodfella47@gmail.com>
2021-01-29 19:38:23 +0200

    refractor and new comments

--- misc/OptimizerTypes.py
+++ misc/OptimizerTypes.py
@@ -6,5 +6,5 @@ class OptimizerTypes(Enum):
     The various algorithms for determine if invoke the tree builder.
     """
     TRIVIAL = 0,
-    CHANGED_BY_T = 1,
+    CHANGES_AWARE = 1,
     USING_INVARIANT = 2
