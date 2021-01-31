9c2e6956e8531f1f26e1470f2817c85ff60437ad
Robert Spektor <goodfella47@gmail.com>
2021-01-31 22:22:41 +0200

    deleted comments

--- statistics_collector/StatisticEventData.py
+++ statistics_collector/StatisticEventData.py
@@ -2,13 +2,6 @@ from datetime import timedelta, datetime
 from base.Event import Event
 
 
-# class StatisticEventData:
-#
-#     def __init__(self, timestamp: timedelta, event_type: str):
-#         self.timestamp = timestamp
-#         self.event_type = event_type
-
-
 class StatisticEventData:
     """
     Contains an event type along with event timestamp
