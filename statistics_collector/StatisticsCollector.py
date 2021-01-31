f89094a676eb09a874da1a656271bc669734ecfc
Robert Spektor <goodfella47@gmail.com>
2021-01-31 19:07:54 +0200

    Change name

--- statistics_collector/StatisticsCollector.py
+++ statistics_collector/StatisticsCollector.py
@@ -6,12 +6,17 @@ from statistics_collector.Statistics import Statistics
 
 
 class StatisticsCollector:
-
+    """
+    Collects, maintains and updates statistics from the stream
+    """
     def __init__(self, pattern: Pattern, statistics: Statistics):
         self.pattern = pattern
         self.__statistics = statistics
 
     def event_handler(self, event: Event):
+        """
+        Updates the statistics with the new event
+        """
         self.__statistics.update(event)
 
     def get_statistics(self):
