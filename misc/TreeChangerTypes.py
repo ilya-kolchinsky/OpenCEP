81a957667efeadc5d1f39efa677c5070932d694d
ofriol <ofriol15@gmail.com>
2021-01-31 18:48:55 +0200

    delete file

--- misc/TreeChangerTypes.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class TreeChangerTypes(Enum):
-    """
-    The various algorithms for determine how to change tree in run time.
-    """
-    TRIVIAL_TREE_CHANGER = 0
-    PARALLEL_TREE_CHANGER = 1
