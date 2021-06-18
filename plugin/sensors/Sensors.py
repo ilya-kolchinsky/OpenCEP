6bdbfccc3d249478b20ec33ebdcbaf9e38a27bb1
Guy-El Fridman <guy-elf@campus.technion.ac.il>
2021-06-18 13:09:44 +0300

    fixed datetime for test

--- plugin/sensors/Sensors.py
+++ plugin/sensors/Sensors.py
@@ -72,24 +72,27 @@ class SensorsDataFormatter(DataFormatter):
 
 if __name__ == '__main__':
     types_list = list(SENSORS_ADDITIONAL_COLUMN_KEYS.keys())
+    cur_time = datetime.now()
     with open(r"..\..\test\EventFiles\Sensors.dat", "w+") as output_file:
         for i in range(1000):
             choice = random.choice(types_list)
-            timestamp = (datetime.now() + timedelta(seconds=i)).strftime("%m/%d/%Y %H:%M:%S")
+            timestamp = (cur_time + timedelta(seconds=i)).strftime("%m/%d/%Y %H:%M:%S")
+            output = choice + "," + timestamp
             if choice == 'PressTemp':
                 temperature = str(round(25 + random.uniform(-2, 2), 3))
                 pressure = str(round(950 + random.uniform(-10, 10), 3))
-                output_file.write(choice + "," + timestamp + "," + temperature + "," + pressure + "\n")
+                output += "," + temperature + "," + pressure + "\n"
             elif choice == 'Accelerometer':
                 accX = str(round(random.uniform(-50, 50), 3))
                 accY = str(round(random.uniform(-100, 100), 3))
                 accZ = str(round(random.uniform(-20, 20), 3))
-                output_file.write(choice + "," + timestamp + "," + accX + "," + accY + "," + accZ + "\n")
+                output += "," + accX + "," + accY + "," + accZ + "\n"
             elif choice == 'Magnetometer':
                 magX = str(round(random.uniform(-50, 50), 3))
                 magY = str(round(random.uniform(-100, 100), 3))
                 magZ = str(round(random.uniform(-20, 20), 3))
-                output_file.write(choice + "," + timestamp + "," + magX + "," + magY + "," + magZ + "\n")
+                output += "," + magX + "," + magY + "," + magZ + "\n"
+            output_file.write(output)
     data_formatter = SensorsDataFormatter()
     with open(r"..\..\test\EventFiles\Sensors.dat", "r") as input_file:
         for line in input_file:
