8aa2300656b71cf6d9ad4e88188ea8286bd13cc1
Shimon Refaelov <srefaelov@gmail.com>
2022-01-27 02:43:11 +0200

    add documentation for the multi pattern graph

--- plan/multi/local_search/MultiPatternGraph.py
+++ plan/multi/local_search/MultiPatternGraph.py
@@ -7,6 +7,12 @@ from base.Pattern import Pattern
 
 
 class MultiPatternGraph:
+    """
+    Data structure that is capable of efficiently retrieving information about the mutual
+     sub patterns between a list of patterns.
+     This is a compact representation of the graph, according to the article:
+     https://assaf.net.technion.ac.il/files/2019/03/Real-Time-Multi-Pattern-Detection-over-Event-Streams.pdf
+    """
     def __init__(self, patterns: List[Pattern]):
         self.__patterns_list = patterns
         self.__pattern_to_maximal_common_sub_patterns = None  # dict of Pattern -> set of max common sub patterns
